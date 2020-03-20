3307c270d1efad0fb3ea2ca72422df55bc9eb15d
chiuko <chiuko@celloes.com>
2020-03-20 14:21:27 +0800

    add default

---
+++ app/Http/Controllers/LineBotController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Request, Curl;
+
+class LineBotController extends Controller
+{
+    public function index(Request $request)
+    {
+    	echo 'hi';
+    }
+}
