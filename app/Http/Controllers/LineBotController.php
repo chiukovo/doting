bdb0f4b3e658fc7355767a4bce453ff552c754af
chiuko <chiuko@celloes.com>
2020-04-01 10:23:55 +0800

    v1

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\SignatureValidator;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use Request, Curl;
 
 class LineBotController extends Controller
@@ -10,5 +15,34 @@ class LineBotController extends Controller
     public function index(Request $request)
     {
     	echo 'hi';
+    }
+
+    public function message(Request $request)
+    {
+        $lineAccessToken = env('LINE_BOT_CHANNEL_ACCESS_TOKEN');
+        $lineChannelSecret = env('LINE_BOT_CHANNEL_SECRET');
+
+        $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
+        if (!SignatureValidator::validateSignature($request->getContent(), $lineChannelSecret, $signature)) {
+            return;
+        }
+
+        $httpClient = new CurlHTTPClient ($lineAccessToken);
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
+
+        try {
+            $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+            foreach ($events as $event) {
+                $replyToken = $event->getReplyToken();
+                $text = $event->getText();// 得到使用者輸入
+           		$lineBot->replyText($replyToken, $text);// 回復使用者輸入
+                $textMessage = new TextMessageBuilder("你好");
+              	$lineBot->replyMessage($replyToken, $textMessage);
+            }
+        } catch (Exception $e) {
+            return;
+        }
+        return;
     }
 }
