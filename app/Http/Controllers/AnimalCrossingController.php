bec2fc94ea8379e9ed1aa0706ca3ed34f1034138
Your Name <you@example.com>
2020-04-21 00:51:01 +0800

    add

--- app/Http/Controllers/AnimalCrossingController.php
+++ app/Http/Controllers/AnimalCrossingController.php
@@ -575,13 +575,87 @@ class AnimalCrossingController extends Controller
             ->setMargin(ComponentMargin::MD)
             ->setFlex(0);
 
-        $south = '';
-        $north = '';
+        $south = $this->getFishMonth($item, '南');
+
+        $components[] = TextComponentBuilder::builder()
+            ->setText('南半球月份: ' . $south)
+            ->setWrap(true)
+            ->setAlign('center')
+            ->setSize(ComponentFontSize::XS)
+            ->setMargin(ComponentMargin::MD)
+            ->setFlex(0);
+
+        $north = $this->getFishMonth($item, '北');
+
+        $components[] = TextComponentBuilder::builder()
+            ->setText('北半球月份: ' . $north)
+            ->setWrap(true)
+            ->setAlign('center')
+            ->setSize(ComponentFontSize::XS)
+            ->setMargin(ComponentMargin::MD)
+            ->setFlex(0);
 
         return BoxComponentBuilder::builder()
             ->setLayout(ComponentLayout::VERTICAL)
             ->setBackgroundColor('#f1f1f1')
             ->setSpacing(ComponentSpacing::SM)
             ->setContents($components);
+    }
+
+    public function getFishMonth($item, $type)
+    {
+        $target = [];
+
+        if ($item->m1 == $type || $item->m1 == '全') {
+            $target[] = 1;
+        }
+
+        if ($item->m2 == $type || $item->m2 == '全') {
+            $target[] = 2;
+        }
+
+        if ($item->m3 == $type || $item->m3 == '全') {
+            $target[] = 3;
+        }
+
+        if ($item->m4 == $type || $item->m4 == '全') {
+            $target[] = 4;
+        }
+
+        if ($item->m5 == $type || $item->m5 == '全') {
+            $target[] = 5;
+        }
+
+        if ($item->m6 == $type || $item->m6 == '全') {
+            $target[] = 6;
+        }
+
+        if ($item->m7 == $type || $item->m7 == '全') {
+            $target[] = 7;
+        }
+
+        if ($item->m8 == $type || $item->m8 == '全') {
+            $target[] = 8;
+        }
+
+        if ($item->m9 == $type || $item->m9 == '全') {
+            $target[] = 9;
+        }
+
+        if ($item->m10 == $type || $item->m10 == '全') {
+            $target[] = 10;
+        }
+
+        if ($item->m11 == $type || $item->m11 == '全') {
+            $target[] = 11;
+        }
+
+        if ($item->m12 == $type || $item->m12 == '全') {
+            $target[] = 12;
+        }
+
+        $string = implode(",", $target);
+
+        return $string;
     }
 }
