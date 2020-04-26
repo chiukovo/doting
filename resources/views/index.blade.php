b9294201ff6ea823134612f9fe413ebc9b3523d6
Quni <quniwork@gmail.com>
2020-04-26 16:41:20 +0800

    quni:20200326

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,14 +29,40 @@
             <ul class="first-list">
               <li>
                 <a href="animals/list">
-                  <img src="../image/icon_animal.png" alt="">
+                  <img src="../image/icon_animal.png" alt="動物居民">
                   <span>動物居民</span>
                 </a>
               </li>
               <li>
                 <a href="animals/list">
-                  <img src="../image/icon_npc.png" alt="">
+                  <img src="../image/icon_npc.png" alt="動物NPC">
                   <span>動物NPC</span>
+                </a>
+              </li>
+              <li>
+                <a href="animals/list">
+                  <img src="../image/icon_museum.png" alt="博物館">
+                  <span>博物館</span>
+                </a>
+              </li>
+            </ul>
+            <ul class="first-list">
+              <li>
+                <a href="animals/list">
+                  <img src="../image/icon_diy.png" alt="DIY方程式">
+                  <span>DIY方程式</span>
+                </a>
+              </li>
+              <li>
+                <a href="animals/list">
+                  <img src="../image/icon_furniture.png" alt="家具">
+                  <span>家具</span>
+                </a>
+              </li>
+              <li>
+                <a href="animals/list">
+                  <img src="../image/icon_clothing.png" alt="服飾">
+                  <span>服飾</span>
                 </a>
               </li>
             </ul>
