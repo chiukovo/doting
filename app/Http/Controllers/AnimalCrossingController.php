9aa38a70370c74f082274701c93c1394fa2744d3
chiuko <chiuko@celloes.com>
2020-04-27 18:01:57 +0800

    fix

--- app/Http/Controllers/AnimalCrossingController.php
+++ app/Http/Controllers/AnimalCrossingController.php
@@ -354,12 +354,12 @@ class AnimalCrossingController extends Controller
         $text .= '範例 查名稱：找貓跳台、找咖啡杯' . "\n";
         $text .= '範例 查名稱：找熱狗、找黃金' . "\n";
         $text .= "\n";
-        $text .= '6.打抽試試運氣' . "\n";
-        $text .= '範例 抽' . "\n";
-        $text .= "\n";
-        $text .= '7.【查】查詢真假畫' . "\n";
+        $text .= '6.【查】查詢真假畫' . "\n";
         $text .= '範例 查充滿母愛的雕塑' . "\n";
         $text .= '範例 查名畫' . "\n";
+        $text .= "\n";
+        $text .= '7.打抽試試運氣' . "\n";
+        $text .= '範例 抽' . "\n";
         $text .= "\n";
         $text .= '歡迎提供缺漏或錯誤修正的資訊，以及功能建議。' . "\n";
         $text .= 'https://www.doting.tw';
