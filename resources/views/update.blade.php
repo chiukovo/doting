c3d840fbe897c23fe922770b97865d2f56c9a11a
Your Name <you@example.com>
2020-05-18 12:35:03 +0800

    add

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -239,16 +239,48 @@
           </div>
         </div>
         <div class="col-12 col-lg-2">
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <ins class="adsbygoogle"
-               style="display:block"
-               data-ad-client="ca-pub-2560043137442562"
-               data-ad-slot="4286195067"
-               data-ad-format="auto"
-               data-full-width-responsive="true"></ins>
-          <script>
-               $(document).ready(function(){(adsbygoogle = window.adsbygoogle || []).push({})})
-          </script>
+          <div class="row">
+            <div class="col">
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              <ins class="adsbygoogle"
+                   style="display:block"
+                   data-ad-client="ca-pub-2560043137442562"
+                   data-ad-slot="4286195067"
+                   data-ad-format="auto"
+                   data-full-width-responsive="true"></ins>
+              <script>
+                   $(document).ready(function(){(adsbygoogle = window.adsbygoogle || []).push({})})
+              </script>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              <ins class="adsbygoogle"
+                   style="display:block"
+                   data-ad-client="ca-pub-2560043137442562"
+                   data-ad-slot="4286195067"
+                   data-ad-format="auto"
+                   data-full-width-responsive="true"></ins>
+              <script>
+                   $(document).ready(function(){(adsbygoogle = window.adsbygoogle || []).push({})})
+              </script>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              <ins class="adsbygoogle"
+                   style="display:block"
+                   data-ad-client="ca-pub-2560043137442562"
+                   data-ad-slot="4286195067"
+                   data-ad-format="auto"
+                   data-full-width-responsive="true"></ins>
+              <script>
+                   $(document).ready(function(){(adsbygoogle = window.adsbygoogle || []).push({})})
+              </script>
+            </div>
+          </div>
         </div>
       </div>
     </section>
