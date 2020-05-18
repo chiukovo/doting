17cd73406110ba71c544eb2a9337795a1bce6b7b
Your Name <you@example.com>
2020-05-18 23:13:12 +0800

    fix

--- resources/views/layouts/ads.blade.php
+++ resources/views/layouts/ads.blade.php
@@ -5,7 +5,7 @@
 		     data-ad-client="ca-pub-2560043137442562"
 		     data-ad-slot="4286195067"
 		     data-ad-format="horizontal"
-		     data-full-width-responsive="false"></ins>
+		     data-full-width-responsive="true"></ins>
 		<script>
 		     $(document).ready(function(){
 		     	setTimeout(function(){
