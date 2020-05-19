52352926648d10fda458ca9e24d1f62d2c0a4462
Your Name <you@example.com>
2020-05-19 21:12:57 +0800

    fix

--- resources/views/layouts/ads.blade.php
+++ resources/views/layouts/ads.blade.php
@@ -10,7 +10,7 @@
 		     $(document).ready(function(){
 		     	setTimeout(function(){
 		     		(adsbygoogle = window.adsbygoogle || []).push({})
-		     	 }, 100)
+		     	 }, 500)
 		     })
 		</script>
 	</div>
