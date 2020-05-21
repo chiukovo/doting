8498f8d3545fc7f8173ff2224ec98f30d0b916e2
chiuko <chiuko@celloes.com>
2020-05-21 11:26:54 +0800

    fix

--- app/Services/AnimalServices.php
+++ app/Services/AnimalServices.php
@@ -403,6 +403,7 @@ class AnimalServices
         $item->race = $item->race != '' ? $item->race : '-';
         $item->bd = $item->bd != '' ? $item->bd : '-';
         $item->say = $item->say != '' ? $item->say : '-';
+        $item->sex = $item->sex != '' ? $item->sex : '-';
         $item->target = $item->target != '' ? $item->target : '-';
 
         //npc
