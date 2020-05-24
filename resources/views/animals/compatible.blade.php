b8f7c8624bd92ecedc102d7a673c4eedd6eef648
Your Name <you@example.com>
2020-05-24 23:51:43 +0800

    fix

--- resources/views/animals/compatible.blade.php
+++ resources/views/animals/compatible.blade.php
@@ -220,7 +220,24 @@
           </div>
           <!--ads-->
           <div class="text-center" v-if="analysis.length > 0">
-            @include('layouts.ads')
+            <div class="row text-center mb-2" style="min-height: 280px">
+              <div class="col">
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-2560043137442562"
+                     data-ad-slot="4286195067"
+                     data-ad-format="auto"
+                     data-full-width-responsive="true"></ins>
+                <script>
+                 $(document).ready(function(){
+                  setTimeout(function(){
+                    (adsbygoogle = window.adsbygoogle || []).push({})
+                   }, 300)
+                 })
+                </script>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
