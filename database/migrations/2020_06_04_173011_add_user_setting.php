d5a7cccff45ac8e961e718c140ae1b8d78792434
chiuko <chiuko@celloes.com>
2020-06-04 17:53:37 +0800

    add

---
+++ database/migrations/2020_06_04_173011_add_user_setting.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserSetting extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('web_user', function ($table) {
+            $table->integer('open_user_data')->default(0)->after('info');
+            $table->integer('open_picture')->default(0)->after('info');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('web_user', function ($table) {
+            $table->dropColumn('open_user_data');
+            $table->dropColumn('open_picture');
+        });
+    }
+}
